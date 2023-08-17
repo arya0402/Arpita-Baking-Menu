5c7f07d23f5ceb2705b88309b84881d19873bb81
Aryadolphin <78508957+arya0402@users.noreply.github.com>
2023-08-17 16:21:47 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+    function f_cakes() {
+        window.location.replace("cakes.html");
+    }
+
+    function f_cupcakes() {
+        window.location.replace("cupcakes.html");
+    }
+
+    function f_cookies() {
+        window.location.replace("cookies.html");
+    }
+
+    function f_tcakes() {
+        window.location.replace("tcakes.html");
+    }
+
+    function f_hcb() {
+        window.location.replace("cakes.html");
+    }
+
+    function back() {
+        window.location.replace("index.html");
+    }
